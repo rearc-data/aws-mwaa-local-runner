4efc8e9f896448e026117b25cdb9b56c9e366c36
David Maxson <david.maxson@rearc.io>
2022-02-02 12:31:58 -0800

    Manually updated for Airflow 2.2.2. This may need to be overridden when AWS officially releases the same upgrade.

--- docker/build_all.sh
+++ docker/build_all.sh
@@ -1,1 +1,1 @@
-docker buildx build --platform linux/amd64,linux/arm64 --cache-from=registry --push -t 658443672529.dkr.ecr.us-east-1.amazonaws.com/amazon/mwaa-local:2.0.2 -f docker/Dockerfile docker
+docker buildx build --pull --platform linux/amd64,linux/arm64 --cache-from=registry --push -t 658443672529.dkr.ecr.us-east-1.amazonaws.com/amazon/mwaa-local:2.2.2 -f docker/Dockerfile docker
